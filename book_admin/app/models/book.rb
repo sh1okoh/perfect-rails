c14cfc6064ff7632d3ef3dfda8ac9f0d04c91f9e
sh1okoh <kooooo.0719task@gmail.com>
2022-01-03 14:17:41 +0900

    note: class method と scope method の挙動の違い。class method は 条件に合致しない場合はnilを返すが scope method は必ずActiveRecord::Relationを返すという動作をする

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, ->(thema) { where("name like ?", "%#{thema}%") }
+  scope :find_price, ->(price) { find_by(price: price) }
 end
