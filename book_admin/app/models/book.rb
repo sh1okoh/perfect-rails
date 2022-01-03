e66b58175a0fe98f94252c877d56803e3635f2eb
sh1okoh <kooooo.0719task@gmail.com>
2022-01-03 15:39:36 +0900

    feat: バリデーションの追加

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -5,4 +5,42 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  # before_validation do
+  #   self.name = self.name.gsub(/Cat/) do |matched|
+  #     "lovely #{matched}"
+  #   end
+  # end
+
+  before_validation :add_lovely_to_cat
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn  "Please check!!"
+  end
+
+  private
+
+  def add_lovely_to_cat
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  def high_price?
+    price >= 5000
+  end
 end
