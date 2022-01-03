e86a23dc02548abf65533892b95c5b44f8e49460
sh1okoh <kooooo.0719task@gmail.com>
2022-01-03 15:06:22 +0900

    多対多を表現

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, ->(thema) { where("name like ?", "%#{thema}%") }
-  scope :find_price, ->(price) { find_by(price: price) }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
