c7d069a84c619aaf09557c147adeb2e7ed7d374f
sh1okoh <kooooo.0719task@gmail.com>
2022-01-03 16:38:15 +0900

    routes

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
   resources :publishers
+  resource :profile, only: %i{show edit update}
 end
