fc705aa8d56190157790d3fab427bfdc88059ab6
sh1okoh <kooooo.0719task@gmail.com>
2022-01-04 19:51:03 +0900

    さまざまな追加

--- book_admin/app/controllers/application_controller.rb
+++ book_admin/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   before_action do
     redirect_to access_denied_path if params[:token].blank?
   end
+
+  rescue_from LoginFailed, with: :login_failed
+
+  private
+
+  def login_failed
+    render template: "shared/login_failed", status: 401
+  end
 end
